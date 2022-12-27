0af64e5fff3b9de5201669f495486b3d92c1250b
吳兆峰 <113499272+110302010@users.noreply.github.com>
2022-12-28 01:23:29 +0800

    Update p00.py

--- exam/p/p00.py
+++ exam/p/p00.py
@@ -1,6 +1,10 @@
 def p00(n=0):
     even=None
     # ↓程式區域↓
-
+num = int
+if (num % 2) == 0:
+   print("{0} true".format(num))
+else:
+   print("{0} flase".format(num))
     # ↑程式區域↑
     return even
