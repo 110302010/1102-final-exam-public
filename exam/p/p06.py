95b9afbd6baff52fcc309b9d1920c22468580578
吳兆峰 <113499272+110302010@users.noreply.github.com>
2022-12-28 01:35:43 +0800

    Update p06.py

--- exam/p/p06.py
+++ exam/p/p06.py
@@ -1,6 +1,21 @@
 def p06(nums = [2,7,11,15], target = 9):
     output_list=None
     # ↓程式區域↓
-
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        for leftIndex in 0 ..< nums.count {
+            
+            let lackValue = target - nums[leftIndex]
+            let nextElementIndex = leftIndex + 1
+            
+            for rightIndex in nextElementIndex ..< nums.count
+                where lackValue == nums[rightIndex] {
+                    return [leftIndex, rightIndex]
+            }
+        }
+        return [-1]
+    }
+}
     # ↑程式區域↑
     return output_list
