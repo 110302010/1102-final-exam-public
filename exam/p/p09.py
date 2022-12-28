7d712b7d43d5e2fb0c68061057c027720817a3d3
吳兆峰 <113499272+110302010@users.noreply.github.com>
2022-12-28 10:36:11 +0800

    Update p09.py

--- exam/p/p09.py
+++ exam/p/p09.py
@@ -1,6 +1,9 @@
 def p09(citations=[]):
     h=None
     # ↓程式區域↓
-
+    citations.sort(reverse=True)
+    for i, c in enumerate(citations):
+        if i >= c:
+            return i
     # ↑程式區域↑
     return h
